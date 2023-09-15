87f850eb81d48d925785911eb60dcfd5436ee9b9
suveshmoza <anmolmoza2@gmail.com>
2023-09-16 01:07:40 +0530

    initial setup

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const AllExpenses = () => {
+	return (
+		<View>
+			<Text>AllExpenses</Text>
+		</View>
+	);
+};
+
+export default AllExpenses;
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ManageExpenses = () => {
+	return (
+		<View>
+			<Text>ManageExpenses</Text>
+		</View>
+	);
+};
+
+export default ManageExpenses;
